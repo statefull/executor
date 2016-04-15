03e9105fc617c0310cd55db2fc670756462ac1ea
Fernando Bernal <fernando.bernal@displaynote.com>
2016-04-15 11:42:54 +0200

    added possibility to execute a work more than one with the possibility of being notificated when the work end

--- execution.cpp
+++ execution.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <functional>
 #include <thread>
-#include <vector>
+#include <queue>
 #include <mutex>
 #include <condition_variable>
 #include <future>
 #include <memory>
+#include <map>
+#include <limits>
 #include <unistd.h>
 
 namespace WORK
@@ -25,40 +27,40 @@ namespace WORK
 
 	class WorkInterface {
 	public:
-		virtual void execute() = 0;
-		virtual void setExecutionConfirmation(std::promise<bool> *promise) = 0;
+		virtual void execute(int index = 0) = 0;
+		virtual void setExecutionConfirmation(std::promise<bool> *promise, int index = 0) = 0;
 	};
 
 	template <typename R, typename... Args>
 	class Work: public WorkInterface {
 	public:
-		Work(std::function<R (Args... params)> work, Args... params): m_work(work), args(std::forward<Args>(params)...), m_workFinished(nullptr) {}
+		Work(std::function<R (Args... params)> work, Args... params): m_work(work), args(std::forward<Args>(params)...) {}
 		~Work() {}
-		void execute() {
-			func(args);
+		void execute(int index = 0) {
+			func(args, index);			
 		}
 	private:
-		template <typename... RArgs, int... Is>
-		void func(std::tuple<RArgs...>& tup, HELPER::index<Is...>)
+		template <typename... RArgs, int... Is, typename Index>
+		void func(std::tuple<RArgs...>& tup, HELPER::index<Is...>, Index index)
 		{
 			m_work(std::get<Is>(tup)...);
-			if(m_workFinished) { m_workFinished->set_value(true); }
+			if(m_workFinished.at(index)) { m_workFinished.at(index)->set_value(true); m_workFinished.erase(index); }
 		}
 
-	    template <typename... RArgs>
-		void func(std::tuple<RArgs...>& tup)
+	    template <typename... RArgs, typename Index>
+		void func(std::tuple<RArgs...>& tup, Index index)
 		{
-			func(tup, HELPER::gen_seq<sizeof...(RArgs)>{});
+			func(tup, HELPER::gen_seq<sizeof...(RArgs)>{}, index);
 		}
 
-		void setExecutionConfirmation(std::promise<bool> *promise)
+		void setExecutionConfirmation(std::promise<bool> *promise, int index = 0)
 		{
-			m_workFinished = promise;
+			m_workFinished[index] = promise;
 		}
 
 		std::function<R (Args... params)> m_work;
 		std::tuple<Args...> args;
-		std::promise<bool> *m_workFinished;
+		std::map<int,std::promise<bool>*> m_workFinished;
 	};
 }
 
@@ -70,18 +72,21 @@ namespace EXECUTOR
 
 		std::future<bool> addWork(WORK::WorkInterface *w)
 		{
-			{
-				std::lock_guard<std::mutex> guard(m_mutexWorks);
-				m_works.push_back(w);
-			}
-			std::promise<bool> *promise = new std::promise<bool>();
-			w->setExecutionConfirmation(promise);
+			std::lock_guard<std::mutex> guard(m_mutexWorks);
+			
+			unsigned int nextIndex = 0;
+			nextIndex = (m_promiseIndex % std::numeric_limits<unsigned int>::max());
+			m_works.push(std::make_pair(nextIndex,w));			
+			
+			std::promise<bool> *promise = new std::promise<bool>();			
+			w->setExecutionConfirmation(promise,nextIndex);
+			m_promiseIndex++;
 			noData.notify_one();
 			return promise->get_future();
 		}
 
 	private:
-		Executor(bool ordered):m_ordered(ordered), m_waitForDestroy(false), m_running(false) { start(); }
+		Executor(bool ordered):m_ordered(ordered), m_waitForDestroy(false), m_running(false), m_promiseIndex(0) { start(); }
 		~Executor() {}
 		Executor( const Executor& other ) = delete;
 		Executor& operator=( const Executor& )  = delete;
@@ -125,29 +130,34 @@ namespace EXECUTOR
 
 				});
 
-				WORK::WorkInterface * w = nullptr;
+				std::pair<int,WORK::WorkInterface *> w;
+				thread_local bool runThread = false;
 
 				{
 					std::lock_guard<std::mutex> guard(m_mutexWorks);
-					if(!m_works.empty()) w = m_works.front();
-				}
-
-				if(w)
-				{
-					std::thread t(&WORK::WorkInterface::execute,w);
-					m_ordered ? t.join() : t.detach();
-
-					{
-						std::lock_guard<std::mutex> guard(m_mutexWorks);
-						m_works.erase(m_works.begin());
+					
+					if(!m_works.empty())
+					{ 
+						w = m_works.front(); 
+						m_works.pop();
+						runThread = true;						
 					}
 				}
+
+				if(runThread)
+				{
+					runThread = false;
+					std::thread t(&WORK::WorkInterface::execute,std::get<1>(w),std::get<0>(w));
+					m_ordered ? t.join() : t.detach();
+				}
+				
+				
 			}while(m_running || !m_works.empty());
 
 			if(!m_waitForDestroy) { delete this; }
 		}
 
-		std::vector<WORK::WorkInterface *> m_works;
+		std::queue<std::pair<unsigned int,WORK::WorkInterface *>> m_works;
 		mutable std::mutex m_mutexWorks;
 		mutable std::mutex m_mutexData;
 		mutable std::mutex m_mutexExecutor;
@@ -156,6 +166,7 @@ namespace EXECUTOR
 		bool m_running;
 		bool m_ordered;
 		bool m_waitForDestroy;
+		unsigned int m_promiseIndex;
 	};
 
 
@@ -199,19 +210,20 @@ class Test {
 public:
 	Test(){}
 	~Test(){}
-	void print(std::string text) { std::cout << text; }
+	void print(std::string text) { std::cout << text << std::endl; }
 };
 
 int main()
 {
 	EXECUTOR::Executor * ex = EXECUTOR::ExecutorFactory::instance().createUnorderedExecution();
-	WORK::Work<void,int,int> w([] (int a,int b) -> void { std::cout << a << b; },12,15);
+	WORK::Work<void,int,int> w([] (int a,int b) -> void { std::cout << a << b << std::endl; },12,15);
 
 	Test t;
 	WORK::Work<void> w1(std::bind(&Test::print,t,"hola"));
 
 	std::future<bool> fut = ex->addWork(&w);
 	std::future<bool> fut2 = ex->addWork(&w1);
+	std::future<bool> fut3 = ex->addWork(&w);
 
 	std::function<void()> workFinished = [&fut] () {
 		if(fut.get()) { std::cout << "work finished" << std::endl; }
@@ -221,8 +233,13 @@ int main()
 		if(fut2.get()) { std::cout << "work finished 2" << std::endl; }
 	};
 
+	std::function<void()> workFinished3 = [&fut3] () {
+		if(fut3.get()) { std::cout << "work finished 3" << std::endl; }
+	};
+
 	std::thread(workFinished).detach();
 	std::thread(workFinished2).detach();
+	std::thread(workFinished3).detach();
 
 	EXECUTOR::ExecutorFactory::instance().removeExecutorNoWaiting(ex);
 
